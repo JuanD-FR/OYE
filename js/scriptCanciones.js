cd42167cf9f617196b7701b615a5b074e9d05d70
Juan David Fernández Rovira <94807346+JuanD-FR@users.noreply.github.com>
2023-06-08 10:09:19 -1200

    Update scriptCanciones.js
    prueba

--- js/scriptCanciones.js
+++ js/scriptCanciones.js
@@ -1,10 +1,8 @@
 document.querySelector(obtenerDatos());
 
 function obtenerDatos(){ 
-    a=sessionStorage.getItem("datosCanciones");
-	console.log(a)
     var listaCan = JSON.parse(sessionStorage.getItem("datosCanciones"));
-    console.log(listaCan)
+    
     $("#can1").attr("src","/canciones/".concat(listaCan.canciones[0].ruta));
     var n1 = document.getElementById('nomb1');
     n1.innerHTML = listaCan.canciones[0].nombre;
